aef2d1c4cec57e142c3838ad62dba2e9093d3f1a
Tommy Palmer <hi@tommyp.org>
2015-12-25 22:53:37 +0000

    Delete a friend

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -4,6 +4,13 @@ export default Ember.Route.extend({
 
   model() {
     return this.store.findAll('friend');
+  },
+
+  actions: {
+    delete(friend) {
+      friend.destroyRecord();
+      return false;
+    }
   }
 
 });
