57eef176ee6dbe84a087b28c9f63802b0a89ae6d
Tommy Palmer <hi@tommyp.org>
2015-12-30 08:29:37 +0000

    Save new articles

--- app/models/article.js
+++ app/models/article.js
@@ -4,6 +4,6 @@ export default DS.Model.extend({
   createdAt: DS.attr('date'),
   description: DS.attr('string'),
   notes: DS.attr('string'),
-  state: DS.attr('string'),
+  state: DS.attr('string', {defaultValue: 'borrowed'}),
   friend: DS.belongsTo('friend'),
 });
