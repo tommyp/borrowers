279a75c738e3922ed8644a884cf3b0d1cbacef2d
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2016-03-22 23:29:55 +0000

    Date helper complete

--- app/routes/articles/index.js
+++ app/routes/articles/index.js
@@ -11,7 +11,7 @@ export default Ember.Route.extend({
     }
   },
 
-  resetController(controller, isExisting) {
+  resetController(controller) {
     var model = controller.get('model');
     if (model.get('isNew')) {
       model.destroyRecord();
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,6 +19,8 @@ module.exports = function(defaults) {
   app.import('bower_components/picnic/releases/plugins.min.css');
   app.import('bower_components/picnic/releases/picnic.min.css');
 
+  app.import('bower_components/moment/moment.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
