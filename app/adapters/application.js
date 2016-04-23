0e0cd3be581ef9efd5c0bd1de81ec3db6efbe89b
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2016-04-23 15:21:52 +0100

    Use V4 API and a promise

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,6 @@ import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
   host: 'http://api.ember-cli-101.com',
-  namespace: 'api/v3',
+  namespace: 'api/v4',
   coalesceFindRequests: true
 });
